c3569f77a8cfce232d249b7b39047d3713d25685
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2021-09-29 11:04:58 -0500

    Importar estudiantes 1

---
+++ application/views/estudiantes/import.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Import/Export using phpspreadsheet in codeigniter</title>
+</head>
+<body>
+<style>
+h3
+{
+font-family: Verdana;
+font-size: 14pt;
+font-style: normal;
+font-weight: bold;
+color:red;
+text-align: center;
+}
+table.tr{
+font-family: Verdana;
+color:black;
+font-size: 12pt;
+font-style: normal;
+font-weight: bold;
+text-align:left;
+}
+</style>
+<h3><u>Import/Export using phpspreadsheet in codeigniter</u></h3>
+<form action="http://localhost/integratic/Imports/importar_estudiantes" method="post" enctype="multipart/form-data">
+<table align="center" cellpadding = "5">
+<tr>
+<td>File :</td>
+<td><input type="file" size="40px" name="upload_file" /></td>
+<td colspan="5" align="center">
+<input type="submit" value="Import Users"/></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
