640d4f66c7ec2223f5166ee26500dcc8d5627fd4
Nikollai Hernandez G <nikollaihernandez@gmail.com>
2023-12-26 10:52:15 -0500

    Email actividad

--- application/views/email/actividad.php
+++ application/views/email/actividad.php
@@ -34,9 +34,10 @@
     <p>Estimado/a <?= $estudiante ?>,</p>
     <p>Te informamos que se ha creado una nueva actividad para el grado <strong><?= $actividad["grado"]. $actividad["grupo"] ?></strong> en la plataforma Integratic. A continuación, se detallan los datos:</p>
     <ul>
-      <li><strong>Nombre de la Actividad:</strong> <?= $actividad["titulo_actividad"] ?></li>
-      <li><strong>Docente Responsable:</strong> <?= $actividad["nombres"]." ". $actividad["apellidos"] ?></li>
-      <li><strong>Disponible desde:</strong> <?= $actividad["disponible_desde"] ?><strong> hasta:</strong> <?= $actividad["disponible_hasta"] ?></li>
+        <li><strong>Materia:</strong> <?= $actividad["nommateria"] ?></li>
+        <li><strong>Nombre de la Actividad:</strong> <?= $actividad["titulo_actividad"] ?></li>
+        <li><strong>Docente Responsable:</strong> <?= $actividad["nombres"]." ". $actividad["apellidos"] ?></li>
+        <li><strong>Disponible desde:</strong> <?= $actividad["disponible_desde"] ?><strong> hasta:</strong> <?= $actividad["disponible_hasta"] ?></li>
     </ul>
     <p>Te animamos a revisar la plataforma para obtener más detalles y asegurarte de estar preparado/a para la actividad.</p>
     <p>¡Gracias por tu atención!</p>
